94a1f5e1abaa31d91fdd260927c3731d14079a98
Marya <masha.smelkowa@yandex.ru>
2024-05-10 23:12:17 +0300

    corrected commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double weight, double height) {
-      double bmi = weight / (height * height); // вычисляем индекс массы тела
+        double bmi = weight / (height * height); // вычисляем индекс массы тела
 
         return (int) bmi;
     }
